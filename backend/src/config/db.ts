280c6ce841aa6f04eca42ce77b46d30af2a18546
quyetpm2004 <phammanhquyetx9@gmail.com>
2026-01-09 18:48:33 +0700

    feat: set up Docker and Docker Compose for backend and frontend services

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,19 +1,18 @@
 // Get the client
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 // Create the connection to database
 
-
 const getConnection = async () => {
-    const connection = await mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'nodejspro',
-    });
+  const connection = await mysql.createConnection({
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
-    return connection
-}   
+  return connection;
+};
 
-export default getConnection
+export default getConnection;
